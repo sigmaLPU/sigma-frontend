2863639faefceda4f76edb32b41a04634bde6e13
animesh9893 <animesh.shrivatri2002@gmail.com>
2022-08-20 00:32:19 +0530

    frontend almost done

--- src/components/default_pages/LoginPage.js
+++ src/components/default_pages/LoginPage.js
@@ -8,7 +8,10 @@ import React from 'react';
 // other imports
 import BackgroundImage from './resource/login_background.jpg'
 import MailIcon from './resource/mail.svg'
-
+import PasswordIcon from './resource/lock.svg'
+import HideIcon from './resource/hide.svg'
+import CheckBoxIcon from './resource/checkbox.svg'
+import LPULogo from './resource/lpu_logo.svg'
 
 // function defination
 
@@ -26,7 +29,7 @@ function LoginCard(props){
 	const logoCSS = {
 		width: "396.73px",
 		height: "138px",
-		border:"1px solid black",
+		// border:"1px solid black",
 		marginTop:"41px",
 	}
 
@@ -42,7 +45,7 @@ function LoginCard(props){
 	return (
 		<div style={style}>
 			<div style={logoCSS}>
-				Logo
+				<img src={LPULogo} style={{height:"100%",width:"100%"}} />				
 			</div>
 			<div style={{marginTop:"79px"}}>
 				<div style={{...inputCSS,marginBottom:"17px"}}>
@@ -59,7 +62,28 @@ function LoginCard(props){
 							onChange={(e)=>{console.log(e.target.value)}}/>
 				</div>
 				<div style={inputCSS}>
-					emailCSS
+					<img src={PasswordIcon} style={{width:"24px",height:"24px",marginLeft: "15px"}}/>
+					<input type="text" placeholder="Password" 
+						style={{
+								width:"90%",border: "none",
+								backgroundColor: "transparent",
+								resize: "none",
+								outline: "none",
+								fontSize:"20px",
+								padding:"4px",
+							}} 
+							onChange={(e)=>{console.log(e.target.value)}}/>
+					<img src={HideIcon} style={{width:"24px",height:"24px",marginRight: "15px"}}/>
+				</div>
+				<div style={{marginTop:"40px",display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+					<div style={{fontSize:"20px",fontWeight:"900",display:"flex",color:"#747980",alignItems:"center"}}>
+						<div style={{width:"16px",height:"16px",border: "2px solid #667085",borderRadius:"4px"}}></div>
+						<span style={{marginLeft:"8px"}}>Remember Me</span>
+					</div>
+					<div style={{color:"#3665C5",fontSize:"20px",fontWeight:"900"}}>Forget Password</div>
+				</div>
+				<div style={{marginTop:"79px",width: "460.33px",height: "56px",background:"#F07F1A",borderRadius:"6px",display:"flex",justifyContent:"center",alignItems:"center"}}>
+					<span style={{fontWeight:900,fontSize:"20px",color:"white"}}>Login</span>
 				</div>
 			</div>
 		</div>
