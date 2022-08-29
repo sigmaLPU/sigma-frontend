986ec18e31f4ac18523d79aff7a0eb653b6ef313
animesh9893 <animesh.shrivatri2002@gmail.com>
2022-08-29 15:09:23 +0530

    404 page updated

--- src/components/default_pages/errors/Error404.js
+++ src/components/default_pages/errors/Error404.js
@@ -6,15 +6,45 @@ import React from 'react';
 
 
 // other imports
-
+import RejcetImg from '../resource/404reject.png'
 
 
 // function defination
 
 export default function Error404(props){
+
+	const word = ["S","I","G","M","A"]
+	const _404 = ["4","0","4"]
 	return (
-		<div>
-			BAD REQUEST 404 ERROR
+		<div style={{width:"100%",height:"100vh",display:"flex",justifyContent:"center",alignItems:"center"}}>
+			<div style={{display:"flex"}}>
+				<div style={{}}>
+					<img src={RejcetImg} alt="reject img"/>
+				</div>
+				<div style={{display:"flex",flexDirection:"column"}}>
+					<div style={{display:"flex",alignItems:"stretch",minWidth:"21rem"}}>
+					{
+						word.map((item)=>(
+							<div style={{width:"154px",backgroundColor:"#F07F1A",height:"165px",border:"9px solid #000000",marginRight:"41px"}}>
+								<span style={{fontSize:"128px",fontWeight:"700",color:"white",display:"flex",justifyContent:"center",alignItems:"center"}}>{item}</span>
+							</div>
+						))
+					}	
+					</div>
+					<div style={{display:"flex",flexDirection:"column",alignItems:"center",width:"100%",height:"100%"}}>
+						<div style={{fontSize:"128px",fontWeight:"700",color:"black"}}>
+							{
+								["4","0","4"].map((item,key)=>(
+									<span style={key!==2 ? {marginRight:"140px"} : {}}>{item}</span>
+								))
+							}
+						</div>
+						<div style={{fontSize:"71px",fontWeight:"700"}}>Page Not Found</div>
+						<div style={{fontSize:"1.6rem"}}>ਤੁਹਾਨੂੰ ਇੱਥੇ ਆਉਣ ਦੀ ਇਜਾਜ਼ਤ ਨਹੀਂ ਹੈ</div>
+						<div style={{fontSize:"1.2rem"}}>आपको यहां रहने की अनुमति नहीं है |</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
