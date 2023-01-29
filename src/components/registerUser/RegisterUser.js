26ea315e479b3046994c0a236c3fd4d73cbb983c
syket-das <saiketsd23@gmail.com>
2023-01-29 14:03:17 +0530

    user

--- src/components/registerUser/RegisterUser.js
+++ src/components/registerUser/RegisterUser.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavSideBarLayout } from '../routes';
-import { Button, TextField } from '@mui/material';
+import { Box, Button, Dialog, DialogContent, IconButton, TextField } from '@mui/material';
 import axios from 'axios';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { Link } from 'react-router-dom';
+import { display } from '@mui/system';
+import { AddCircle, Delete, Edit } from '@mui/icons-material';
 
 const RegisterUser = () => {
   const [name, setName] = React.useState('');
@@ -9,86 +13,248 @@ const RegisterUser = () => {
   const [password, setPassword] = React.useState('123456');
   const [regNo, setRegNo] = React.useState('');
 
-  const submitHandler = async(e) => {
-    e.preventDefault();
-    const formData = new FormData();
+  const [users, setUsers] = React.useState([]);
 
-    formData.append('name', name);
-    formData.append('email', email);
-    formData.append('regNo', regNo);
-    formData.append('password', password);
 
-    await axios.post(
-      'https://sigma-lpu-vsbd9.ondigitalocean.app/api/v2/user/register',
-      {
-        name,
-        email,
-        regNo,
-        password
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const { data } = await axios.get(
+          'https://sigma-lpu-vsbd9.ondigitalocean.app/api/v2/user/getAllUsers',
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + localStorage.getItem('token'),
+            },
+          }
+        );
+
+        setUsers(data && data.users);
+      } catch (error) {
+        alert(error && error.response.data.error);
       }
-
+    }
+    fetchUser();
+  }, []);
     
-    
-    );
+  const [open, setOpen] = React.useState(false);
 
-  
+  const handleClickOpen = (aggrement) => {
+    setOpen(true);
   };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await axios.post(
+        'https://sigma-lpu-vsbd9.ondigitalocean.app/api/v2/user/register',
+
+        {
+          name,
+          email,
+          regNo,
+          password,
+        }
+      );
+
+      alert(data && data.data.message);
+    } catch (error) {
+      alert(error && error.response.data.error);
+    }
+  };
+
+  const columns = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      cellClassName: 'name-column--cell',
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      flex: 1,
+    },
+    {
+      field: 'regNo',
+      headerName: 'UID / Reg No',
+      flex: 1,
+    },
+    {
+      field: 'authorization',
+      headerName: 'Role',
+      flex: 1,
+    },
+
+    {
+      field: 'details',
+      headerName: 'Actions',
+      flex: 0.5,
+      renderCell: (cellValue) => {
+        return (
+          <div
+            style={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-around',
+            }}
+          >
+            <IconButton>
+              <Edit />
+            </IconButton>
+            <IconButton>
+              <Delete />
+            </IconButton>
+          </div>
+        );
+      },
+    },
+  ];
 
   return (
     <div>
-      <NavSideBarLayout childCSS={{ marginTop: '1.5rem' }}></NavSideBarLayout>
-      <h3 style={{ textAlign: 'center' }}>Register User - Admin</h3>
-      <div
-        style={{
-          marginTop: '1.5rem',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <form
-        onSubmit={submitHandler}
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            rowGap: '1rem',
-            width: '607px',
-          }}
-        >
-          <TextField
-            id="outlined-basic"
-            label="Enter Name"
-            variant="outlined"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Enter Email"
-            variant="outlined"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-          type='number'
-            id="outlined-basic"
-            label="UID / Reg No"
-            variant="outlined"
-            value={regNo}
-            onChange={(e) => setRegNo(e.target.value)}
-          />{' '}
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button  type="submit" variant="contained" color="primary">
-            Register
-          </Button>
-        </form>
-      </div>
+      <NavSideBarLayout childCSS={{ marginTop: '1.5rem' }}>
+        <div style={{ display: 'flex' }}>
+          <div style={{ marginLeft: '1rem', width: '100%' }}>
+            <Box
+              display="flex"
+              justifyContent="flex-end"
+              sx={{
+                position: 'relative',
+                bottom: '-30px',
+              }}
+            >
+              <Button
+            disabled={users.length === 0}
+                sx={{
+                  backgroundColor: '#F07F1A',
+
+                  color: '#000',
+                  fontSize: '14px',
+                  fontWeight: 'bold',
+                  padding: '10px 20px',
+                }}
+                endIcon={<AddCircle />}
+                onClick={handleClickOpen}
+              >
+                Add New
+              </Button>
+            </Box>
+
+            <Box
+              m="20px 0 0 0"
+              height="85vh"
+              sx={{
+                '& .MuiDataGrid-root': {
+                  border: 'none',
+                },
+                '& .MuiDataGrid-cell': {
+                  borderBottom: 'none',
+                },
+                '& .name-column--cell': {
+                  color: '#F07F1A',
+                },
+                '& .MuiDataGrid-columnHeaders': {
+                  backgroundColor: '#F07F1A',
+                  borderBottom: 'none',
+                },
+                '& .MuiDataGrid-virtualScroller': {
+                  // backgroundColor: "#F07F1A",
+                },
+                '& .MuiDataGrid-footerContainer': {
+                  borderTop: 'none',
+                  backgroundColor: '#F07F1A',
+                },
+                '& .MuiCheckbox-root': {
+                  color: `#F07F1A !important`,
+                },
+                '& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+                  color: `#F07F1A !important`,
+                },
+              }}
+            >
+              <DataGrid
+                rows={users || []}
+                columns={columns}
+                getRowId={(row) => row._id}
+                components={{ Toolbar: GridToolbar }}
+                checkboxSelection
+                // use multiple filters
+              />
+            </Box>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              fullWidth={true}
+              maxWidth={'md'}
+            >
+              <DialogContent
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              >
+                <div>
+                  <h3 style={{ textAlign: 'center' }}>Register User - Admin</h3>
+                  <div
+                    style={{
+                      marginTop: '1.5rem',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <form
+                      onSubmit={submitHandler}
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        rowGap: '1rem',
+                        width: '607px',
+                      }}
+                    >
+                      <TextField
+                        label="Enter Name"
+                        variant="outlined"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                      <TextField
+                        label="Enter Email"
+                        variant="outlined"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <TextField
+                        type="number"
+                        label="UID / Reg No"
+                        variant="outlined"
+                        value={regNo}
+                        onChange={(e) => setRegNo(e.target.value)}
+                      />{' '}
+                      <TextField
+                        label="Password"
+                        variant="outlined"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      <Button type="submit" variant="contained" color="primary">
+                        Register
+                      </Button>
+                    </form>
+                  </div>
+                </div>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
+      </NavSideBarLayout>
     </div>
   );
 };
--- src/components/universityProfile/UniversityMaster.js
+++ src/components/universityProfile/UniversityMaster.js
@@ -22,18 +22,10 @@ import { AddNewUniversity } from '../routes';
 // other imports
 import {
   getAllUniversityReducer,
-  // setRedirectFunction,
-  // updateViewDetails,
-  // activateYourTagChip,
-  // deleteYourTagChip,
-  // addYourTagChip,
+
 } from '../../redux/routes';
 import {
-  // getAllMeetingsSlice,
-  // getAllUniversityMeetingsReducer,
-  // universityMeetingsAddReducer,
-  // setRedirectFunctionMeetings,
-  // updateViewDetailsMeetings,
+
 } from '../../redux/routes';
 import { AddCircle } from '@mui/icons-material';
 
@@ -50,61 +42,17 @@ export default function MouMaster(props) {
     console.log(data.data.rows);
   }, [rawData, data.data.rows]);
 
-  // const replace = [
-  //   {
-  //     name: 'Details',
-  //     value: function (id) {
-  //       return (
-  //         <div
-  //           onClick={() => redirectTo(id)}
-  //           style={{
-  //             color: '#F07F1A',
-  //             width: '100%',
-  //             display: 'flex',
-  //             justifyContent: 'center',
-  //             alignItems: 'center',
-  //             cursor: 'pointer',
-  //           }}
-  //         >
-  //           Details
-  //         </div>
-  //       );
-  //     },
-  //   },
-  // ];
+
 
   useEffect(() => {
     dispatch(getAllUniversityReducer({}));
   }, []);
 
   const navigate = useNavigate();
-  // table state
-  // const [message, setMessage] = useState('loading the tables');
-  const [column, setColumn] = useState([
-    'Name of University',
-    'Country',
-    'Meetings',
-    'Contact Person',
-    'Agreement',
-    'Details',
-  ]);
-  const [rows, setRows] = useState([]);
 
-  // const ChipCSS = {
-  //   minWidth: '126px',
-  //   height: '28px',
-  //   background: '#FFFFFF',
-  //   border: '1px solid #F07F1A',
-  //   borderRadius: '7px',
-  //   margin: '4px',
-  // };
-  // const CardCSS = {
-  //   minHeight: '418px',
-  //   width: '372px',
-  //   border: '1px solid #F07F1A',
-  //   boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.25)',
-  //   color: 'black',
-  // };
+ 
+
+  
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = (aggrement) => {
@@ -115,27 +63,8 @@ export default function MouMaster(props) {
     setOpen(false);
   };
 
-  // function getData() {
-  //   return {
-  //     pagenation_id: '1234',
-  //     column: column,
-  //     rows: rows,
-  //   };
-  // }
-
-  // function redirectTo(id) {
-  //   const url = `/university/${id}`;
-  //   navigate(url);
-  // }
-
-  // function ToggleChip(array, setArray, id) {
-  //   var arr = array;
-  //   arr[id].active = !arr[id].active;
-  //   setArray(arr);
-  // }
 
   useEffect(() => {
-    // ToggleChip(yourTags,setYourTags,0)
   }, []);
 
   const columns = [
@@ -193,33 +122,10 @@ export default function MouMaster(props) {
     <div>
       <NavSideBarLayout childCSS={{ marginTop: '1.5rem' }}>
         <div style={{ display: 'flex' }}>
-          {/*Left part*/}
-          {/* <div style={{ borderRight: '1px solid #D9D9D9', maxWidth: '372px' }}>
-            <div>
-              <input
-                type="text"
-                placeholder="Search"
-                style={{
-                  width: '372px',
-                  fontSize: '20px',
-                  marginBottom: '2rem',
-                }}
-              />
-            </div>
-
-            <YourTags />
-            <PopularTags />
-          </div> */}
-          {/*Right Part*/}
+       
+     
           <div style={{ marginLeft: '1rem', width: '100%' }}>
-            {/* <Table 
-							data={data}
-							rows = {data?.data?.rows}
-							column = {data?.data?.column}
-							heading={"Partner University"}
-							replace = {replace}
-							popup = {<AddNewUniversity />}
-						/> */}
+       
             <Box
               display="flex"
               justifyContent="flex-end"
@@ -307,98 +213,3 @@ export default function MouMaster(props) {
     </div>
   );
 }
-
-// function YourTags(props) {
-//   const rawData = useSelector((state) => state.tagsMouMasterSlice.data);
-
-//   useEffect(() => {}, []);
-
-//   const ChipCSS = {
-//     minWidth: '126px',
-//     height: '28px',
-//     background: '#FFFFFF',
-//     border: '1px solid #F07F1A',
-//     borderRadius: '7px',
-//     margin: '4px',
-//   };
-//   const CardCSS = {
-//     minHeight: '218px',
-//     width: '372px',
-//     border: '1px solid #F07F1A',
-//     boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.25)',
-//     color: 'black',
-//   };
-
-//   const [yourTags, setYourTags] = useState([
-//     { title: 'USA', active: false },
-//     { title: 'India', active: false },
-//     { title: 'Canada', active: false },
-//     { title: 'Bhutan', active: false },
-//     { title: 'Nepal', active: true },
-//   ]);
-
-//   return (
-//     <Card
-//       style={CardCSS}
-//       heading={'Your Tags'}
-//       styleHeading={{ color: 'black' }}
-//     >
-//       <div
-//         style={{
-//           width: '100%',
-//           height: '100%',
-//           display: 'flex',
-//           flexWrap: 'wrap',
-//         }}
-//       >
-//         {yourTags.map((item, key) => (
-//           <Chip style={ChipCSS} text={item.title} active={item.active} />
-//         ))}
-//       </div>
-//     </Card>
-//   );
-// }
-
-// function PopularTags(props) {
-//   const ChipCSS = {
-//     minWidth: '126px',
-//     height: '28px',
-//     background: '#FFFFFF',
-//     border: '1px solid #F07F1A',
-//     borderRadius: '7px',
-//     margin: '4px',
-//   };
-//   const CardCSS = {
-//     minHeight: '218px',
-//     width: '372px',
-//     border: '1px solid #F07F1A',
-//     boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.25)',
-//     color: 'black',
-//   };
-
-//   // tags state
-//   const [popularTags, setPopularTags] = useState([
-//     { title: 'USA', active: false },
-//     { title: 'India', active: false },
-//     { title: 'Canada', active: false },
-//     { title: 'Bhutan', active: false },
-//     { title: 'Nepal', active: true },
-//   ]);
-
-//   return (
-//     <Card style={{ ...CardCSS, marginTop: '2rem' }} heading={'Popular Tags'}>
-//       <div
-//         style={{
-//           width: '100%',
-//           height: '100%',
-//           display: 'flex',
-//           flexWrap: 'wrap',
-//         }}
-//       >
-//         {popularTags.map((item, key) => (
-//           <Chip style={ChipCSS} text={item.title} active={item.active} />
-//         ))}
-//       </div>
-//     </Card>
-//   );
-// }
