3c7b535017de13a5771dcce9f270a696cf305cc0
aakashaman <aakashaman9931@gmail.com>
2023-05-03 18:13:01 +0530

    drive is working!!!!!!!!!!!!!!!

--- src/components/shared_folder/Shared.jsx
+++ src/components/shared_folder/Shared.jsx
@@ -3,9 +3,9 @@ import { NavSideBarLayout } from "../routes";
 export default function Shared() {
   return (
     <div>
-    <NavSideBarLayout childCSS={{ marginTop: '1.5rem' }}>
+    <NavSideBarLayout childCSS={{ marginTop: '3rem' }}>
     <iframe
-      src="https://drive.google.com/drive/folders/1v-_Tur-UFxoJ3ctjDWzkUOe4nFceh5Ki?usp=sharing"
+      src="https://google-drive-master.vercel.app/"
       width="100%"
       height="600"
       frameborder="0"
