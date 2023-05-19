6cc0b565fed249d49d71d61938fc35ebda247980
aakashaman <aakashaman9931@gmail.com>
2023-05-19 16:42:03 +0530

    added icon

--- src/components/home_page/Categoriesnew.js
+++ src/components/home_page/Categoriesnew.js
@@ -2,6 +2,13 @@ import { Button, Card, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import HandshakeIcon from '@mui/icons-material/Handshake';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import SchoolIcon from '@mui/icons-material/School';
+import RateReviewIcon from '@mui/icons-material/RateReview';
+import ModelTrainingIcon from '@mui/icons-material/ModelTraining';
+
 
 export default function Categoriesnew() {
   const navigate = useNavigate();
@@ -40,36 +47,40 @@ export default function Categoriesnew() {
         
         }}
       >
-        <Card>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <Typography
-              variant="h6"
-              component="h5"
-              style={{ marginLeft: "20px", marginTop: "20px" }}
-            >
-              Open Leads
-            </Typography>
-            <Typography
-              align="center"
-              variant="h3"
-              component="h5"
-              style={{ marginLeft: "30px", fontWeight: "bold" }}
-            >
-              100
-            </Typography>
-            <Button
-              onClick={() => navigate("/openLeads")}
-              variant="contained"
-              color="primary"
-              style={{ marginLeft: "20px", marginTop: "20px" }}
-            >
-              <Typography variant="h6" component="h2">
-                View All
-              </Typography>
-            </Button>
-          </div>
-        </Card>
-        <Card>
+      <Card style={{ minWidth: 250, minHeight: 200 }}>
+        
+  <div style={{ display: "flex", flexDirection: "row",justifyContent:"space-between",  marginRight: "30px" }}>
+  <div style={{ backgroundColor: "#F07F1A", padding: "10px",height:55,width:55}}>
+      <LeaderboardIcon fontSize="large"/>
+    </div>
+
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Typography variant="h6" component="h5" style={{ marginLeft: "20px", marginTop: "20px" }}>
+        Open Leads
+      </Typography>
+      <Typography align="center" variant="h3" component="h5" style={{ marginLeft: "30px", fontWeight: "bold" }}>
+        100
+      </Typography>
+      <Button
+        onClick={() => navigate("/openLeads")}
+        variant="contained"
+        color="primary"
+        style={{ marginLeft: "20px", marginTop: "20px",minWidth:100, minHeight: 40 }}
+      >
+        <Typography variant="h6" component="h2">
+          View All
+        </Typography>
+      </Button>
+    </div>
+  </div>
+</Card>
+
+<Card style={{ minWidth: 250, minHeight: 200 }}>
+        
+        <div style={{ display: "flex", flexDirection: "row",justifyContent:"space-between",  marginRight: "30px" }}>
+        <div style={{ backgroundColor: "#F07F1A", padding: "10px",height:55,width:55}}>
+      <HandshakeIcon  fontSize="large" />
+    </div>
           <div style={{ display: "flex", flexDirection: "column" }}>
             <Typography
               variant="h6"
@@ -97,9 +108,15 @@ export default function Categoriesnew() {
               </Typography>
             </Button>
           </div>
+          </div>
         </Card>
 
-        <Card>
+        <Card style={{ minWidth: 250, minHeight: 200 }}>
+        
+  <div style={{ display: "flex", flexDirection: "row",justifyContent:"space-between",  marginRight: "30px" }}>
+  <div style={{ backgroundColor: "#F07F1A", padding: "10px",height:55,width:55}}>
+      <AccountBalanceIcon fontSize="large" />
+    </div>
           <div style={{ display: "flex", flexDirection: "column" }}>
             <Typography
               variant="h6"
@@ -131,9 +148,15 @@ export default function Categoriesnew() {
               </Typography>
             </Button>
           </div>
+          </div>
         </Card>
 
-        <Card>
+        <Card style={{ minWidth: 250, minHeight: 200 }}>
+        
+        <div style={{ display: "flex", flexDirection: "row",justifyContent:"space-between",  marginRight: "30px" }}>
+        <div style={{ backgroundColor: "#F07F1A", padding: "10px",height:55,width:55}}>
+      <SchoolIcon fontSize="large" />
+    </div>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
           <Typography
             variant="h6"
@@ -161,8 +184,14 @@ export default function Categoriesnew() {
             </Typography>
           </Button>
         </div>
+        </div>
         </Card>
-       <Card>
+        <Card style={{ minWidth: 250, minHeight: 200 }}>
+        
+        <div style={{ display: "flex", flexDirection: "row",justifyContent:"space-between",  marginRight: "30px" }}>
+        <div style={{ backgroundColor: "#F07F1A", padding: "10px",height:55,width:55}}>
+      <RateReviewIcon fontSize="large"/>
+    </div>
        <div style={{ display: 'flex', flexDirection: 'column' }}>
           <Typography
             variant="h6"
@@ -189,6 +218,7 @@ export default function Categoriesnew() {
               View All
             </Typography>
           </Button>
+        </div>
         </div>
        </Card>
 
