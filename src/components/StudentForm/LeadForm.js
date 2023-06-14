96bfc2e4fbd00f5950198a46c6636cf88ed780a0
aakashaman <aakashaman9931@gmail.com>
2023-06-14 11:03:41 +0530

    form

--- src/components/StudentForm/LeadForm.js
+++ src/components/StudentForm/LeadForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import banner from "./banner2.gif";
 import {
   Box,
@@ -11,6 +11,29 @@ import {
 } from "@material-ui/core";
 
 export default function LeadForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    registrationNumber: "",
+    email: "",
+    studyAbroadOption: "",
+    currentCourse: "",
+    currentSemester: "",
+    passingOutYear: "",
+  });
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div>
       <Box
@@ -55,69 +78,95 @@ export default function LeadForm() {
           marginTop: "20px",
         }}
       >
-        <TextField
-          className="field"
-          label="Name"
-          variant="outlined"
-          margin="normal"
-          style={{ width: "45%" }}
-        />
-        <TextField
-          className="field"
-          label="Registration Number"
-          variant="outlined"
-          margin="normal"
-          style={{ width: "45%" }}
-        />
-        <TextField
-          className="field"
-          label="Email ID"
-          variant="outlined"
-          margin="normal"
-          style={{ width: "45%" }}
-        />
-        <FormControl
-          className="field"
-          variant="outlined"
-          margin="normal"
-          style={{ width: "45%" }}
-        >
-          <InputLabel>Study Abroad Option</InputLabel>
-          <Select label="Study Abroad Option">
-            <MenuItem value="option1">semester Exchange</MenuItem>
-            <MenuItem value="option2">credit Transfer</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          className="field"
-          label="Current Course"
-          variant="outlined"
-          margin="normal"
-          style={{ width: "45%" }}
-        />
-        <TextField
-          className="field"
-          label="Current Semester"
-          variant="outlined"
-          margin="normal"
-          style={{ width: "45%" }}
-        />
-        <TextField
-          className="field"
-          label="Passing Out Year"
-          variant="outlined"
-          margin="normal"
-          style={{ width: "45%" }}
-        />
-        <Button
-          className="field"
-          variant="contained"
-          color="primary"
-          size="large"
-          style={{ width: "45%", marginTop: "20px" }}
-        >
-          Submit
-        </Button>
+        <form style={{ width: "100%" }} onSubmit={handleFormSubmit}>
+          <TextField
+            className="field"
+            label="Name"
+            variant="outlined"
+            margin="normal"
+            style={{ width: "45%" }}
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <TextField
+            className="field"
+            label="Registration Number"
+            variant="outlined"
+            margin="normal"
+            style={{ width: "45%" }}
+            name="registrationNumber"
+            value={formData.registrationNumber}
+            onChange={handleChange}
+          />
+          <TextField
+            className="field"
+            label="Email ID"
+            variant="outlined"
+            margin="normal"
+            style={{ width: "45%" }}
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <FormControl
+            className="field"
+            variant="outlined"
+            margin="normal"
+            style={{ width: "45%" }}
+          >
+            <InputLabel>Study Abroad Option</InputLabel>
+            <Select
+              label="Study Abroad Option"
+              name="studyAbroadOption"
+              value={formData.studyAbroadOption}
+              onChange={handleChange}
+            >
+              <MenuItem value="option1">semester Exchange</MenuItem>
+              <MenuItem value="option2">credit Transfer</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            className="field"
+            label="Current Course"
+            variant="outlined"
+            margin="normal"
+            style={{ width: "45%" }}
+            name="currentCourse"
+            value={formData.currentCourse}
+            onChange={handleChange}
+          />
+          <TextField
+            className="field"
+            label="Current Semester"
+            variant="outlined"
+            margin="normal"
+            style={{ width: "45%" }}
+            name="currentSemester"
+            value={formData.currentSemester}
+            onChange={handleChange}
+          />
+          <TextField
+            className="field"
+            label="Passing Out Year"
+            variant="outlined"
+            margin="normal"
+            style={{ width: "45%" }}
+            name="passingOutYear"
+            value={formData.passingOutYear}
+            onChange={handleChange}
+          />
+          <Button
+            className="field"
+            variant="contained"
+            color="primary"
+            size="large"
+            style={{ width: "45%", marginTop: "20px" }}
+            type="submit"
+          >
+            Submit
+          </Button>
+        </form>
       </Box>
     </div>
   );
